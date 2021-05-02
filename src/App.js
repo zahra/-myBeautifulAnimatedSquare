7ee7b26a97eb4cd7ae2dae7bb0948a660d0aedb7
Zahra Said <zahra@zahrasaid.org>
2021-05-02 20:40:26 +0200

    Moved square state plus functions to new component 'MySquare'

--- src/App.js
+++ src/App.js
@@ -4,35 +4,6 @@ import React, { useState } from "react";
 import MySquare from "./components/MySquare";
 
 function App() {
-  const [square, setSquare] = useState("App-myBlueSquare");
-  // console.log(square);
-
-  const handleColorChange = () => {
-    // console.log("Please change the value of the state with this new value");
-    // console.log(square);
-
-    let switchColor;
-    if (square === "App-myBlueSquare") {
-      switchColor = "App-myRedSquare";
-      console.log("is this red");
-    } else {
-      switchColor = "App-myBlueSquare";
-      console.log("is this blue");
-    }
-    setSquare(switchColor);
-
-    // if (square === "App-myBlueSquare") {
-    //   switchColor = "App-myRedSquare";
-    // } else {
-    //   switchColor = "App-myBlueSquare";
-    // }
-    // setSquare(switchColor);
-
-    // let newColor =
-    //   square === "App-myBlueSquare" ? "App-myRedSquare" : "App-myBlueSquare";
-    // setSquare(newColor);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
